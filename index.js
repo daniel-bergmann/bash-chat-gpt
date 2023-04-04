3d9b3db0f2f8a5b1b2d598d32cd3c062fff2bf47
daniel-bergmann <danielbergmann@ok.is>
2023-04-04 19:40:38 +0000

    the bot is cleaner now

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { config } from "dotenv"
 import { Configuration, OpenAIApi } from "openai"
 import readline from "readline"
-
 config()
 
 const openai = new OpenAIApi(
@@ -12,36 +11,39 @@ const ui = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 })
+const bot = `
+               _
+              [ ]
+             (   )
+              |>|
+           __/===\__
+          //| o=o |\\
+        <]  | o=o |  [>
+          
 
-const fillerFeedback = [
-  "I've heard that one before. Let's see...",
-  "Hmm... interesting question.",
-  "Let me google that for you, you lazy bum.",
-  "Open AI invested all that money into me and this is the question you ask me? Ok, one moment...",
-]
+  ---Well, hello there. I'm a bot. How can I help?---
+`
 
 ui.prompt()
+console.log(bot)
 ui.on("line", async (input) => {
   const res = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: input }],
   })
-  console.log(fillerFeedback[Math.floor(Math.random() * fillerFeedback.length)])
-  setTimeout(() => {
-    console.log(res.data.choices[0].message.content)
-  }, 3000)
+
+  const message = `
+  
+    <]  | o=o |  [>
+      
+    
+  ---${res.data.choices[0].message.content}
+    
+    
+    <]  | o=o |  [>
+`
+
+  console.log(message)
 
   ui.prompt()
 })
-
-// openai
-//   .createChatCompletion({
-//     model: "gpt-3.5-turbo",
-//     messages: [
-//       { role: "user", content: "how do i make water resistant water?" },
-//     ],
-//   })
-//   .then((res) => {
-//     console.log(res.data.usage)
-//     console.log(res.data.choices[0].message.content)
-//   })
